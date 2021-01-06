5eeb57cd1e4c37639d0e9bcc0fff0c0c56adcf65
wangruxin <435063555@qq.com>
2021-01-06 13:57:29 +0800

    初始提交

---
+++ spring-cloud-config-client-3355/src/main/java/com/wrx/SpringCloudConfigClient_3355.java
@@ -0,0 +1,11 @@
+package com.wrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClient_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClient_3355.class,args);
+    }
+}
